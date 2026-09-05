748b666872de8f81ea652c8a140dd0b0f25eb8e1
Nazym <nazym@MacBook-Air-Nazym-2.local>
2026-09-05 14:55:52 +0500

    Practice 1 Spring Boot project with hello endpoint

---
+++ src/main/java/com/example/practicee1/Practicee1Application.java
@@ -0,0 +1,13 @@
+package com.example.practicee1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practicee1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practicee1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practicee1/Practicee1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicee1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practicee1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
